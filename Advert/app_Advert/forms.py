0baa573cf7e51561e3a3191f33cafc4cff985e75
mathilda-github <mathilda-github@yandex.ru>
2023-08-31 00:37:28 +0300

    Урок Формы (задание 4)

--- Advert/app_Advert/forms.py
+++ Advert/app_Advert/forms.py
@@ -1,8 +1,29 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from .models import Advertisement
 
-class AdvertisementForm(forms.Form):
-    title = forms.CharField(max_length=64, widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}))
-    descr = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control form-control-lg'}))
-    price = forms.DecimalField(widget=forms.NumberInput(attrs={'class': 'form-control form-control-lg'}))
-    auction = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}))
-    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control form-control-lg'}))
+# class AdvertisementForm(forms.Form):
+#     title = forms.CharField(max_length=64, widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}))
+#     descr = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control form-control-lg'}))
+#     price = forms.DecimalField(widget=forms.NumberInput(attrs={'class': 'form-control form-control-lg'}))
+#     auction = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}))
+#     image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control form-control-lg'}))
+
+
+class AdvertisementForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs['class'] = 'form-control form-control-lg'
+        self.fields['descr'].widget.attrs['class'] = 'form-control form-control-lg'
+        self.fields['price'].widget.attrs['class'] = 'form-control form-control-lg'
+        self.fields['auction'].widget.attrs['class'] = 'form-check-input'
+        self.fields['image'].widget.attrs['class'] = 'form-control form-control-lg'
+    class Meta:
+        model = Advertisement
+        fields = ['title', 'descr', 'price', 'auction', 'image']
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if title.startwith('?'):
+            raise ValidationError('not ?')
+        return title
---
+++ eee.py
@@ -0,0 +1,5 @@
+
+def ask_26(a, b):
+    return a>b
+
+print(ask_26(10,16))
